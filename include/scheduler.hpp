3d0389f50fe7c4c4af13a7426d6988dacc067519
Bruno Emanoel <brunoemanoel.l.x@gmail.com>
2026-06-25 13:36:08 -0300

    docs: adds comments in schedulers functions definitions

--- include/scheduler.hpp
+++ include/scheduler.hpp
@@ -94,15 +94,44 @@ public:
 
 class EDFScheduler: public AbstractScheduler {
 public:
+    /** @brief Scheduler using Earliest Deadline First priority
+     * 
+     * Processes are scheduler preemptively so that the process with 
+     * earliest deadline will be executed first. 
+     * Preemption ocurrs when a new process is created and its deadline 
+     * is earlier than the one of the current running process.
+     */
     EDFScheduler(const ScheduleConfiguration &config);
 
+    /**
+     * @brief Executes the schedule using EDF ordering.
+     *
+     * @return ExecutionSchedule containing the ordered execution timeline.
+     */
     ExecutionSchedule execute();
 };
 
 class CFSSimScheduler: public AbstractScheduler {
 public:
-    
+    /** @brief Scheduler using Completely Fair Scheduler - Simplified algorithm
+     * 
+     * Processes are scheduled preemptively so that all of them get a 
+     * fair share of cpu time.
+     * This is ensured through the use of virtual runtimes (vtimes)
+     * that say how many time has the processes been executing.
+     * After a granular amount of time (taken as the quantum) the scheduler 
+     * will check for the process with the least vtime and if it is less than
+     * the running one, then preemption happens.
+     * The initial vtime of a process is its arrival time and it then
+     * goes up by the tame it was in the cpu times a weght function that grows
+     * exponentialy with the priority value (higher equals less priority).
+     */
     CFSSimScheduler(const ScheduleConfiguration &config);
 
+    /**
+     * @brief Executes the schedule using CFS-Sim ordering.
+     *
+     * @return ExecutionSchedule containing the ordered execution timeline.
+     */
     ExecutionSchedule execute();
 };
